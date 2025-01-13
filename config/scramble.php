9348366fb3381fed61356184b9fe06e23963563d
Amanmyrat <tekemuradov@gmail.com>
2025-01-13 14:48:04 +0500

    Add scramble accept language

--- config/scramble.php
+++ config/scramble.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Extensions\AddLocalizationHeaderExtension;
 use Dedoc\Scramble\Http\Middleware\RestrictedDocsAccess;
 
 return [
@@ -83,5 +84,7 @@ return [
         RestrictedDocsAccess::class,
     ],
 
-    'extensions' => [],
+    'extensions' => [
+        AddLocalizationHeaderExtension::class
+    ],
 ];
